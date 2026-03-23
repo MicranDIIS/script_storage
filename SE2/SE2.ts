568d0e667357425e59780d648663c2d1987ee38f
Yegor Ryabtsunov <regor@micran.ru>
2026-03-23 17:01:04 +0700

    Add SE2 moudle for GUI example

---
+++ SE2/SE2.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="102"/>
+        <location filename="main.cpp" line="107"/>
+        <location filename="main.cpp" line="110"/>
+        <source>IMPORTANT</source>
+        <translation>ВАЖНО</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="102"/>
+        <source>Launcher remove - fail</source>
+        <translation>Ошибка удаления Launcher`a</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="107"/>
+        <source>Launcher update - fail</source>
+        <translation>Ошибка обновления Launcher&apos;a</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="110"/>
+        <source>Launcher update - successfull
+Programm will be restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Launcher update - successfull
+Programm will be restart</source>
+        <translation type="obsolete">Launcher успешно обновлён\r\n Программа будет перезапущена</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="225"/>
+        <source>Incorrect launch way</source>
+        <translation>Неправильный способ запуска</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="225"/>
+        <source>Please run Launcher</source>
+        <translation>Пожалуйста, запустете Launcher</translation>
+    </message>
+</context>
+<context>
+    <name>SE2</name>
+    <message>
+        <location filename="se2.cpp" line="241"/>
+        <source>Error BasePlugin.dll</source>
+        <translation>Cannot load BasePlugin.dll</translation>
+    </message>
+    <message>
+        <location filename="se2.cpp" line="323"/>
+        <source>SE2</source>
+        <translation>SE2</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="32"/>
+        <source>Script Executor 2</source>
+        <translation>Менеджер скриптов 2</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="64"/>
+        <source>Categories:</source>
+        <translation>Категории:</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="87"/>
+        <source>Launch</source>
+        <translation>Запустить</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="128"/>
+        <source>New script</source>
+        <translation>Новый скрипт</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="131"/>
+        <location filename="se2.ui" line="160"/>
+        <location filename="se2.ui" line="189"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="157"/>
+        <source>Edit script</source>
+        <translation>Редактировать скрипт</translation>
+    </message>
+    <message>
+        <location filename="se2.ui" line="186"/>
+        <source>Delete script</source>
+        <translation>Удалить скрипт</translation>
+    </message>
+    <message>
+        <location filename="se2.cpp" line="184"/>
+        <location filename="se2.cpp" line="188"/>
+        <source>Other</source>
+        <translation>Другое</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="obsolete">Ошибка</translation>
+    </message>
+    <message>
+        <location filename="se2.cpp" line="324"/>
+        <source>Do you really want to delete this script?</source>
+        <translation>Вы действительно хотите удалить этот скрипт?</translation>
+    </message>
+</context>
+</TS>
