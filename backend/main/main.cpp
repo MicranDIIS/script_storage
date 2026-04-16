bb52d6e4fcbbf34a6fe2b8a1e8197a30f240a0b6
Михаил <mixailochka@mail.ru>
2026-04-16 15:49:03 +0300

    git fetch

--- backend/main/main.cpp
+++ backend/main/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     git_libgit2_init();
 
 
-    Repo repo = Repo::clone("https://github.com/MicranDIIS/script_storage","","","");
+    /*Repo repo = Repo::clone("https://github.com/MicranDIIS/script_storage","","","");
     if(!repo.valid()){
         printf("error\n");
     }else{
@@ -40,10 +40,23 @@ int main(int argc, char *argv[])
         if(value.flag_check(STATUS_NEW_TO_DIR)){
             qDebug() << value.get_path_new() + " " + "NEW_TO_DIR";
         }
+    }*/
+
+    Repo repo = Repo::open("C:\\Users\\user\\testing","");
+
+    if(!repo.valid()){
+        printf("no valid!");
+    }else{
+        if(repo.fetch() != -1){
+            printf("yes!");
+        }else{
+            printf("no!");
+        }
+
     }
 
     git_libgit2_shutdown();
 
-    fclose(f);
+    //fclose(f);
     return a.exec();
 }
--- backend/network/network.cpp
+++ backend/network/network.cpp
@@ -1,6 +1,5 @@
 #include "network.h"
 
-//структура для коллбэк ф-ии
 struct GitData{
     QByteArray username;
     QByteArray token;
@@ -8,7 +7,6 @@ struct GitData{
     GitData(const QString &username_,const QString &token_) : username(username_.toUtf8()) , token(token_.toUtf8()) {}
 };
 
-//для git clone,git fetch и других
 static int callback(git_credential **out,const char *url,
                          const char *username_from_url,
                          unsigned int allowed_types,
@@ -29,7 +27,7 @@ static int callback(git_credential **out,const char *url,
 }
 
 
-//реализация git clone
+
 typedef GitData GitClone;
 
 Repo Repo::clone(const QString &URL,const QString &path,const QString &username,const QString &token){
@@ -57,4 +55,62 @@ Repo Repo::clone(const QString &URL,const QString &path,const QString &username,
     git_repository_free(repo);
 
     return Repo(path,token);
+}
+
+typedef GitData GitFetch;
+
+#include <string.h>
+int Repo::fetch(void){
+    git_remote *remote = NULL;
+    git_fetch_options fetchopt = GIT_FETCH_OPTIONS_INIT;
+    git_remote_callbacks callbacks = GIT_REMOTE_CALLBACKS_INIT;
+
+    callbacks.credentials = callback;
+    callbacks.payload = this;
+    fetchopt.callbacks = callbacks;
+    fetchopt.update_fetchhead = 1;
+
+    git_reference *head = NULL;
+    git_buf remote_buf = GIT_BUF_INIT;
+    const char *remote_name = NULL;
+
+    if(git_repository_head(&head,repo) != 0){
+        return -1;
+    }
+
+    const char *branch_name = NULL;
+    if(git_branch_name(&branch_name,head) == 0){
+        if(git_branch_upstream_remote(&remote_buf,repo,branch_name) == 0){
+            remote_name = remote_buf.ptr;
+        }else{
+            remote_name = "origin";
+        }
+    }else{
+        remote_name = "origin";
+    }
+
+    if(!remote_name || strlen(remote_name) == 0){
+        git_reference_free(head);
+        git_buf_free(&remote_buf);
+        return -1;
+    }
+
+    if(git_remote_lookup(&remote,repo,remote_name) != 0){
+        git_reference_free(head);
+        git_buf_free(&remote_buf);
+        return -1;
+    }
+
+    if(git_remote_fetch(remote,NULL,&fetchopt,NULL) != 0){
+        git_remote_free(remote);
+        git_reference_free(head);
+        git_buf_free(&remote_buf);
+        return -1;
+    }
+
+    git_remote_free(remote);
+    git_reference_free(head);
+    git_buf_free(&remote_buf);
+
+    return 0;
 }
