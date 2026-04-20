506f5fc1029d69290bdea414382d70bab27cb42f
VyacheslavBelg <vaceslavcujkov@gmail.com>
2026-04-20 16:02:12 +0700

    Create IRepository.h

---
+++ backend/core/IRepository.h
@@ -0,0 +1,41 @@
+#ifndef IREPOSITORY_H
+#define IREPOSITORY_H
+
+#include <QString>
+#include <QList>
+#include <QDateTime>
+
+struct CommitInfo {
+    QDateTime dateTime;
+    QString author;
+    QString authorEmail;
+    QString commitMessage;
+    QString commitHash;
+};
+
+struct RepoConfig {
+    QString url;
+    QString branch;
+    QString path;
+    QString username;
+    QString token;
+};
+
+class IRepository {
+public:
+    virtual ~IRepository() = default;
+
+    virtual bool clone(const RepoConfig &config) = 0;
+
+    virtual bool pull(const RepoConfig &config) = 0;
+
+    virtual QList<FileStatus> getStatus(const QString &repoPath) = 0;
+
+    virtual QList<CommitInfo> getFileHistory(const QString &filePath) = 0;
+};
+
+
+IRepository* createRepository();
+void destroyRepository(IRepository *repo);
+
+#endif 
