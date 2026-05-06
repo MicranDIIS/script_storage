4113f5be99869aaa06c54c033a3c2496e9464ccf
Михаил <mixailochka@mail.ru>
2026-05-06 10:25:26 +0300

    добавлен git log и интерфейс

--- backend/main/main.cpp
+++ backend/main/main.cpp
@@ -6,14 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     mgit_init();
-
-    IRepository *repo = createRepository();
-    Gerror err = repo->open("");
-    if(!err.succses){
-        qDebug() << err.msg << "\n";
-    }else{
-        qDebug() << "Все хорошо" << "\n";
-    }
+    
 
     mgit_shutdown();
     return a.exec();
